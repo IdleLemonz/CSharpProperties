891d7b4bb793d39c52a6522573e33fc078091140
Simflare <stu23@live.com>
2016-08-09 11:14:29 +0930

    Initial Commit

---
+++ CSharpProperties/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpProperties
+{
+    class TimePeriod
+    {
+        private int days;
+        public int Days
+        {
+            get
+            {
+                return days;
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TimePeriod time = new TimePeriod();
+            Console.WriteLine(time.Days);
+        }
+    }
+}
